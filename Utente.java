d3c591b780a60f95871faf2edf2a5dcd0418e335
joaoalves14 <joaoalves96@icloud.com>
2023-10-02 15:54:53 +0100

    Version 1 - Constructors

---
+++ Utente.java
@@ -0,0 +1,16 @@
+public class Utente {
+
+    public Utente(String nome, int idade, String genero, double altura, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    private String nome;
+    private int idade;
+    private String genero;
+    private double altura;
+    private double peso;
+}
