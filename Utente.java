b38f08c0a048dbac8035eddc07a772624d81c3f0
joaoalves14 <joaoalves96@icloud.com>
2023-10-02 15:58:31 +0100

    Version 2 - Getters and setters

--- Utente.java
+++ Utente.java
@@ -13,4 +13,46 @@ public class Utente {
     private String genero;
     private double altura;
     private double peso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
 }
